acf05355a783bc6c8bcd7d7849843e97df1cb46c
Bearlim <brutchuces@outlook.com>
2022-07-01 19:47:57 -0300

    [Update]

---
+++ FI.AtividadeEntrevista/DAL/Padrao/FI.AcessoDados.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FI.AtividadeEntrevista.DAL
+{
+    internal class AcessoDados
+    {
+        private string stringDeConexao
+        {
+            get
+            {
+                ConnectionStringSettings conn = System.Configuration.ConfigurationManager.ConnectionStrings["BancoDeDados"];
+                if (conn != null)
+                    return conn.ConnectionString;
+                else
+                    return string.Empty;
+            }
+        }
+
+        internal void Executar(string NomeProcedure, List<SqlParameter> parametros)
+        {
+            SqlCommand comando = new SqlCommand();
+            SqlConnection conexao = new SqlConnection(stringDeConexao);
+            int retorno = 1;
+            comando.Connection = conexao;
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.CommandText = NomeProcedure;
+            foreach (var item in parametros)
+                comando.Parameters.Add(item);
+
+            conexao.Open();
+            try
+            {
+                comando.ExecuteNonQuery();
+                retorno = (Int32)comando.Parameters["RETORNO"].Value;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+            if (retorno == -1)
+                throw new Exception("CPF já cadastrado. Entre em contato com o suporte!");
+        }
+
+        internal DataSet Consultar(string NomeProcedure, List<SqlParameter> parametros)
+        {
+            SqlCommand comando = new SqlCommand();
+            SqlConnection conexao = new SqlConnection(stringDeConexao);
+
+            comando.Connection = conexao;
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.CommandText = NomeProcedure;
+            foreach (var item in parametros)
+                comando.Parameters.Add(item);
+
+            SqlDataAdapter adapter = new SqlDataAdapter(comando);
+            DataSet ds = new DataSet();
+            conexao.Open();
+
+            try
+            {               
+                adapter.Fill(ds);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+            return ds;
+        }
+        internal DataSet ConsultarV2(string NomeProcedure, List<SqlParameter> parametros, out int OutputValue)
+        {
+            SqlCommand comando = new SqlCommand();
+            SqlConnection conexao = new SqlConnection(stringDeConexao);
+
+            comando.Connection = conexao;
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.CommandText = NomeProcedure;
+            foreach (var item in parametros)
+                comando.Parameters.Add(item);
+
+            SqlDataAdapter adapter = new SqlDataAdapter(comando);
+            DataSet ds = new DataSet();
+            conexao.Open();
+
+            try
+            {
+                adapter.Fill(ds);
+                if(comando.Parameters["RETORNO"].Value.GetType().GetProperties().Count() != 0)
+                    OutputValue = (Int32)comando.Parameters["RETORNO"].Value;
+                else 
+                    OutputValue = 1;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+            return ds;
+        }
+    }
+}
---
+++ FI.WebAtividadeEntrevista/Models/BeneficiarioModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAtividadeEntrevista.Models
+{
+    public class BeneficiarioModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public int CPF { get; set; }
+        [Required]
+        public string NOME { get; set; }
+    }
+}
